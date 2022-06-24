f83d2b8c1ee75ae05832becd17cffcea171a498c
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2022-06-24 18:11:21 +0100

    Initial Commit

---
+++ WhatFlagIsThis/WhatFlagIsThisApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatFlagIsThisApp.swift
+//  WhatFlagIsThis
+//
+//  Created by Richard Nkanga on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct WhatFlagIsThisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
