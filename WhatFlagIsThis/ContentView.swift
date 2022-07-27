10cc23152a97459965a2485543a4ec68d4f958fc
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2022-07-27 08:57:18 +0800

    coded the ui

--- WhatFlagIsThis/ContentView.swift
+++ WhatFlagIsThis/ContentView.swift
@@ -8,15 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+  
+
+    
+    @State var countries = ["Estonia","France","Germany","Ireland", "Italy", "Monaco","Nigeria","Poland","Russia","Spain","UK","US"].shuffled()
+    
+        
+    @State var correctAnswer = Int.random(in: 0...2)
+    
+    
+ 
+    
     var body: some View {
         
-        
+     
         ZStack {
-            Text("Hello, world!")
-                .padding()
+            Color.blue.ignoresSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                    Text("Your Score is \(score) ")
+                        .foregroundColor(.white)
+                }
+        
+            
+            ForEach(0..<3){ number in
+                Button {
+                    
+                    flagtapped(number)
+                    
+                    
+                    
+                } label: {
+                    Image(countries[number])
+                        .renderingMode(.original)
+                }
+            }
+                
+            }
         }
+        .alert(scoreTitle, isPresented: $showingScore){
+            Button("Continue", action: askQuestion)
+        }message: {
+            Text("Your score is \(score)")
+        }
+        
+    
+        }
+    
+    func flagtapped(_ number: Int ){
+        if number == correctAnswer{
+            scoreTitle = "Correct Answer"
+            score += 1
+    }else{
+        scoreTitle = "Wrong Answer"
+        
     }
-}
+        
+        showingScore = true
+    }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        
+        
+      
+    }
+        
+        
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
